80f1e9b56d3cbb6bc0c8c13820db9b024da3340d
KooJianYang <jianyangkoo@gmail.com>
2021-11-26 03:18:06 +0800

    Create TryDaiToken.sol

---
+++ contracts/TryDaiToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract TryDaiToken is ERC20, ERC20Permit{
+    address public admin;
+    
+    constructor() ERC20('TryDaiToken', 'TDT') ERC20Permit("Dai Coin"){
+        _mint(msg.sender, 10000 *10 **18);
+        admin = msg.sender;
+    }
+    
+    function mint(address to, uint amount) external{
+        require(msg.sender == admin, "Only admin allowed");
+        _mint(to, amount);
+    }
+    
+    function burn(uint amount) external{
+        _burn(msg.sender, amount);
+    }
+}
